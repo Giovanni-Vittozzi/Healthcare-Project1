e5490090f6054bad1e71317af0b7dd1d63cc3aa7
Giovanni <gvittozzi1@pride.hofstra.edu>
2020-09-07 14:23:09 -0400

    Initial Commit

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Healthcare_Companion.Models;
+
+namespace Healthcare_Companion.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult PatientInfoRegistration()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Models/Patient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Healthcare_Companion.Models
+{
+    public class Patient
+    {
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string Address { get; set; }
+        [Display(Name = "Address 2")]
+        public string Address2 { get; set; }
+        [Display(Name = "Apt./Suite Number")]
+        public string AptNum { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string State { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+    }
+}
