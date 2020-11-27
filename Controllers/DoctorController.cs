ba51f27d84dbe000a47b702ed4c046f24a7ac7d0
Giovanni <gvittozzi1@pride.hofstra.edu>
2020-11-27 11:11:15 -0500

    Updated Buttons on Chart Pages

--- Controllers/DoctorController.cs
+++ Controllers/DoctorController.cs
@@ -126,6 +126,82 @@ namespace HealthcareCompanion.Controllers
             tier.approvePatient(id);
             return RedirectToAction("ApprovePatients", "Doctor");
         }
+        [HttpGet]
+        public ActionResult ChartBloodSugar(int id)
+        {
+            PatientTier tier        = new PatientTier();
+            MedicalData medicalData = new MedicalData();
+            if (Request.IsAuthenticated)
+            {
+                var          userStore   = new UserStore<IdentityUser>();
+                var          userManager = new UserManager<IdentityUser>(userStore);
+                IdentityUser theUser     = userManager.FindById(User.Identity.GetUserId());
+                string       userID      = theUser.Id;
+                medicalData.PatientID    = id;
+                medicalData.TypeID       = 1;
+            }
+            List<MedicalData> medicalDataList = tier.listMedicalDataByTypeID(medicalData.PatientID, medicalData.TypeID);
+            List<string>      MonthList       = new List<string>();
+            List<int>         YearList        = new List<int>();
+            ViewBag.medicalDataList           = medicalDataList;
+            if (medicalDataList != null)
+            {
+                foreach (var item in medicalDataList)
+                {
+                    DateTime date = new DateTime(2020, item.Now.Month, 1);
+                    MonthList.Add(date.ToString("MMMM"));
+                    YearList.Add(item.Now.Year);
+                }
+                MonthList = MonthList.Distinct().ToList();
+                YearList  = YearList.Distinct().ToList();
+                ViewBag.MonthList = MonthList;
+                ViewBag.YearList  = YearList;
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ChartBloodSugar()
+        {
+            PatientTier tier     = new PatientTier();
+            string      month    = Request.Form["MonthSelect"].ToString(); //Get selected month to change data to
+            int?        monthInt = Convert.ToDateTime(month + " 01, 1900").Month;
+            string      year     = Request.Form["YearSelect"].ToString(); //Get selected month to change data to
+            int         yearInt  = Int32.Parse(year);
+            return RedirectToAction("ChartBloodSugarByMonth", new { yearInt = yearInt, monthInt= monthInt});
+        }
+        [HttpGet]
+        public ActionResult ChartBloodSugarByMonth(MedicalData medicalData, int? monthInt, int yearInt)
+        {
+            PatientTier tier     = new PatientTier();
+            if (Request.IsAuthenticated)
+                {
+                    var          userStore   = new UserStore<IdentityUser>();
+                    var          userManager = new UserManager<IdentityUser>(userStore);
+                    IdentityUser theUser     = userManager.FindById(User.Identity.GetUserId());
+                    string       userID      = theUser.Id;
+                    medicalData.PatientID    = tier.getPatientByID(userID);
+                    medicalData.TypeID       = 1;
+            }
+            List<MedicalData> medicalDataList = tier.listMedicalDataByTypeIDByDate(medicalData.PatientID, medicalData.TypeID, monthInt, yearInt);
+            ViewBag.medicalDataList           = medicalDataList;
+            List<MedicalData> monthDataList   = tier.listMedicalDataByTypeID(medicalData.PatientID, medicalData.TypeID);
+            List<string>      MonthList       = new List<string>();
+            List<int>         YearList        = new List<int>();
+            if (medicalDataList != null)
+            {
+                foreach (var item in monthDataList)
+                {
+                    DateTime date = new DateTime(2020, item.Now.Month, 1);
+                    MonthList.Add(date.ToString("MMMM"));
+                    YearList.Add(item.Now.Year);
+                }
+                MonthList = MonthList.Distinct().ToList();
+                YearList = YearList.Distinct().ToList();
+                ViewBag.MonthList = MonthList;
+                ViewBag.YearList = YearList;
+            }
+            return View();
+        }
         [HttpPost]
         public ActionResult SignOut()
         {
