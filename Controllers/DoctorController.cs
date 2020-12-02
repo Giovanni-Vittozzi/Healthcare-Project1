c37557f9025e47b3ac0826a5b0095c59c9432fca
Giovanni <gvittozzi1@pride.hofstra.edu>
2020-12-02 13:03:05 -0500

    Doctor Medical Pages done aside from putting the patient email link on the top of the page

--- Controllers/DoctorController.cs
+++ Controllers/DoctorController.cs
@@ -27,6 +27,27 @@ namespace HealthcareCompanion.Controllers
             return View();
         }
         [HttpGet]
+        public ActionResult ShowProfile()
+        {
+            DoctorTier tier   = new DoctorTier();
+            Doctor     doctor = new Doctor();
+            //need to get current signed in doctor
+            if (Request.IsAuthenticated)
+            {
+                var          userStore   = new UserStore<IdentityUser>();
+                var          userManager = new UserManager<IdentityUser>(userStore);
+                IdentityUser theUser     = userManager.FindById(User.Identity.GetUserId());
+                string       userEmail   = theUser.Email;
+                string       userID      = theUser.Id;
+                var          pendingDoc  = tier.isPendingDoctor(userEmail);
+                doctor.DoctorID          = tier.getDoctorByID(userID);
+                doctor                   = tier.retrieveDoctor(doctor.DoctorID);
+                ViewBag.doctor           = doctor;
+                return View(doctor);
+            }
+            return RedirectToAction("Index", "Doctor");
+        }
+        [HttpGet]
         public ActionResult EditDoctorInfo()
         {
             DoctorTier tier   = new DoctorTier();
@@ -61,7 +82,7 @@ namespace HealthcareCompanion.Controllers
                     doctor.DoctorID       = tier.getDoctorByID(userID);
                     tier.updateDoctorInfo(doctor);
                 }
-                return RedirectToAction("Index", "Doctor");
+                return RedirectToAction("ShowProfile", "Doctor");
             }
             return View();
         }
@@ -268,7 +289,7 @@ namespace HealthcareCompanion.Controllers
                     var          userManager = new UserManager<IdentityUser>(userStore);
                     IdentityUser theUser     = userManager.FindById(User.Identity.GetUserId());
                     string       userID      = theUser.Id;
-                    medicalData.PatientID    = tier.getPatientByID(userID);
+                    medicalData.PatientID    = patientID;
                     medicalData.TypeID       = 2;
             }
             List<MedicalData> medicalDataList = tier.listMedicalDataByTypeIDByDate(medicalData.PatientID, medicalData.TypeID, monthInt, yearInt);
@@ -302,6 +323,181 @@ namespace HealthcareCompanion.Controllers
             int         yearInt  = Int32.Parse(year);
             return RedirectToAction("ChartBloodPressureByMonth", "Doctor", new { yearInt = yearInt, monthInt= monthInt, patientID = patientID});
         }
+        [HttpGet]
+        public ActionResult ChartPulse(int patientID, MedicalData medicalData)
+        {
+            PatientTier tier = new PatientTier();
+            if (Request.IsAuthenticated)
+            {
+                var          userStore   = new UserStore<IdentityUser>();
+                var          userManager = new UserManager<IdentityUser>(userStore);
+                IdentityUser theUser     = userManager.FindById(User.Identity.GetUserId());
+                string       userID      = theUser.Id;
+                medicalData.PatientID    = patientID;
+                medicalData.TypeID       = 3;
+            }
+            List<MedicalData> medicalDataList = tier.listMedicalDataByTypeID(medicalData.PatientID, medicalData.TypeID);
+            List<string>      MonthList       = new List<string>();
+            List<int>         YearList        = new List<int>();
+            ViewBag.medicalDataList           = medicalDataList;
+            if (medicalDataList != null)
+            {
+                foreach (var item in medicalDataList)
+                {
+                    DateTime date = new DateTime(2020, item.Now.Month, 1);
+                    MonthList.Add(date.ToString("MMMM"));
+                    YearList.Add(item.Now.Year);
+                }
+                MonthList = MonthList.Distinct().ToList();
+                YearList  = YearList.Distinct().ToList();
+                ViewBag.MonthList = MonthList;
+                ViewBag.YearList  = YearList;
+                ViewBag.Patient   = patientID;
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ChartPulse(int patientID)
+        {
+            PatientTier tier     = new PatientTier();
+            string      month    = Request.Form["MonthSelect"].ToString(); //Get selected month to change data to
+            int?        monthInt = Convert.ToDateTime(month + " 01, 1900").Month;
+            string      year     = Request.Form["YearSelect"].ToString(); //Get selected month to change data to
+            int         yearInt  = Int32.Parse(year);
+            return RedirectToAction("ChartPulseByMonth", new { yearInt = yearInt, monthInt= monthInt, patientID = patientID});
+        }
+        [HttpGet]
+        public ActionResult ChartPulseByMonth(MedicalData medicalData, int? monthInt, int yearInt, int patientID)
+        {
+            PatientTier tier     = new PatientTier();
+            if (Request.IsAuthenticated)
+                {
+                    var          userStore   = new UserStore<IdentityUser>();
+                    var          userManager = new UserManager<IdentityUser>(userStore);
+                    IdentityUser theUser     = userManager.FindById(User.Identity.GetUserId());
+                    string       userID      = theUser.Id;
+                    medicalData.PatientID    = patientID;
+                    medicalData.TypeID       = 3;
+            }
+            List<MedicalData> medicalDataList = tier.listMedicalDataByTypeIDByDate(medicalData.PatientID, medicalData.TypeID, monthInt, yearInt);
+            ViewBag.medicalDataList           = medicalDataList;
+            List<MedicalData> monthDataList   = tier.listMedicalDataByTypeID(medicalData.PatientID, medicalData.TypeID);
+            List<string>      MonthList       = new List<string>();
+            List<int>         YearList        = new List<int>();
+            if (medicalDataList != null)
+            {
+                foreach (var item in monthDataList)
+                {
+                    DateTime date = new DateTime(2020, item.Now.Month, 1);
+                    MonthList.Add(date.ToString("MMMM"));
+                    YearList.Add(item.Now.Year);
+                }
+                MonthList = MonthList.Distinct().ToList();
+                YearList  = YearList.Distinct().ToList();
+                ViewBag.MonthList = MonthList;
+                ViewBag.YearList  = YearList;
+                ViewBag.Patient   = patientID;
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ChartPulseByMonth(int patientID)
+        {
+            PatientTier tier     = new PatientTier();
+            string      month    = Request.Form["MonthSelect"].ToString(); //Get selected month to change data to
+            int?        monthInt = Convert.ToDateTime(month + " 01, 1900").Month;
+            string      year     = Request.Form["YearSelect"].ToString(); //Get selected month to change data to
+            int         yearInt  = Int32.Parse(year);
+            return RedirectToAction("ChartPulseByMonth", new { yearInt = yearInt, monthInt= monthInt, patientID = patientID});
+        }
+        [HttpGet]
+        public ActionResult ChartWeight(MedicalData medicalData, int patientID)
+        {
+            PatientTier tier = new PatientTier();
+            if (Request.IsAuthenticated)
+            {
+                var          userStore   = new UserStore<IdentityUser>();
+                var          userManager = new UserManager<IdentityUser>(userStore);
+                IdentityUser theUser     = userManager.FindById(User.Identity.GetUserId());
+                string       userID      = theUser.Id;
+                medicalData.PatientID    = patientID;
+                medicalData.TypeID       = 4;
+            }
+            List<MedicalData> medicalDataList = tier.listMedicalDataByTypeID(medicalData.PatientID, medicalData.TypeID);
+            List<string>      MonthList       = new List<string>();
+            List<int>         YearList        = new List<int>();
+            ViewBag.medicalDataList           = medicalDataList;
+            if (medicalDataList != null)
+            {
+                foreach (var item in medicalDataList)
+                {
+                    DateTime date = new DateTime(2020, item.Now.Month, 1);
+                    MonthList.Add(date.ToString("MMMM"));
+                    YearList.Add(item.Now.Year);
+                }
+                MonthList = MonthList.Distinct().ToList();
+                YearList = YearList.Distinct().ToList();
+                ViewBag.MonthList = MonthList;
+                ViewBag.YearList  = YearList;
+                ViewBag.Patient   = patientID;
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ChartWeight(int patientID)
+        {
+            PatientTier tier     = new PatientTier();
+            string      month    = Request.Form["MonthSelect"].ToString(); //Get selected month to change data to
+            int?        monthInt = Convert.ToDateTime(month + " 01, 1900").Month;
+            string      year     = Request.Form["YearSelect"].ToString(); //Get selected month to change data to
+            int         yearInt  = Int32.Parse(year);
+            return RedirectToAction("ChartWeightByMonth", new { yearInt = yearInt, monthInt= monthInt, patientID = patientID});
+        }
+        [HttpGet]
+        public ActionResult ChartWeightByMonth(MedicalData medicalData, int? monthInt, int yearInt, int patientID)
+        {
+            PatientTier tier     = new PatientTier();
+            if (Request.IsAuthenticated)
+            {
+                    var          userStore   = new UserStore<IdentityUser>();
+                    var          userManager = new UserManager<IdentityUser>(userStore);
+                    IdentityUser theUser     = userManager.FindById(User.Identity.GetUserId());
+                    string       userID      = theUser.Id;
+                    medicalData.PatientID    = patientID;
+                    medicalData.TypeID       = 4;
+            }
+            List<MedicalData> medicalDataList = tier.listMedicalDataByTypeIDByDate(medicalData.PatientID, medicalData.TypeID, monthInt, yearInt);
+            ViewBag.medicalDataList           = medicalDataList;
+            List<MedicalData> monthDataList   = tier.listMedicalDataByTypeID(medicalData.PatientID, medicalData.TypeID);
+            List<string>      MonthList       = new List<string>();
+            List<int>         YearList        = new List<int>();
+            if (medicalDataList != null)
+            {
+                foreach (var item in monthDataList)
+                {
+                    DateTime date = new DateTime(2020, item.Now.Month, 1);
+                    MonthList.Add(date.ToString("MMMM"));
+                    YearList.Add(item.Now.Year);
+                }
+                MonthList = MonthList.Distinct().ToList();
+                YearList  = YearList.Distinct().ToList();
+                ViewBag.MonthList = MonthList;
+                ViewBag.YearList  = YearList;
+                ViewBag.Patient   = patientID;
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ChartWeightByMonth(int patientID)
+        {
+            PatientTier tier     = new PatientTier();
+            string      month    = Request.Form["MonthSelect"].ToString(); //Get selected month to change data to
+            int?        monthInt = Convert.ToDateTime(month + " 01, 1900").Month;
+            string      year     = Request.Form["YearSelect"].ToString(); //Get selected month to change data to
+            int         yearInt  = Int32.Parse(year);
+            return RedirectToAction("ChartWeightByMonth", new { yearInt = yearInt, monthInt= monthInt, patientID = patientID});
+        }
+
         [HttpPost]
         public ActionResult SignOut()
         {
